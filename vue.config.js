d9f4572ef08d7b4ddfcac15aaf5b40481d2cf3d7
Suukee <jiangyunbh@gmail.com>
2020-11-30 23:18:47 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
         productName: 'uniecloud-desktop',
         copyright: 'Copyright©unicloud',
         directories: {
-          output: './dist_electron' // 输出文件路径
+          output: './dist_electron', // 输出文件路径
         },
         win: {
           // win相关配置
@@ -18,14 +18,15 @@ module.exports = {
             {
               target: 'nsis', // 不打包为安装程序
               arch: [
-                'x64' //64位
+                'x64', //64位
                 // "ia32", //32位
-              ]
-            }
-          ]
-        }
-      }
-    }
+              ],
+            },
+          ],
+        },
+        publish: ['github'],
+      },
+    },
   },
   css: {
     loaderOptions: {
